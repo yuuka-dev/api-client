917e3d280b62164968fcda3f907276fea6ab7b0e
yuuka <yuuka@example.com>
2026-03-24 12:49:23 +0900

    ci: add PyPI publish workflow and update author email

--- onlinejudge/__about__.py
+++ onlinejudge/__about__.py
@@ -1,9 +1,9 @@
 # Python Version: 3.x
-__package_name__ = 'online-judge-api-client'
-__author__ = 'Kimiyuki Onaka'
-__email__ = 'kimiyuki95@gmail.com'
+__package_name__ = 'online-judge-api-client-ng'
+__author__ = 'yuuka-dev'
+__email__ = 'admin@osaka29.jp'
 __license__ = 'MIT License'
-__url__ = 'https://github.com/online-judge-tools/api-client'
+__url__ = 'https://github.com/yuuka-dev/api-client'
 __version_info__ = (10, 11, 0, 'final', 0)
 __version__ = '.'.join(map(str, __version_info__[:3]))
 __description__ = 'API client to develop tools for competitive programming'
